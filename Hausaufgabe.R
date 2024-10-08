9a09fcc70206d543e8b6036de0beaf210d6e2c2e
Julian Hildebrandt <super-dude@gmx.de>
2024-10-08 10:27:01 +0200

    Update Hausaufgabe.R

--- Hausaufgabe.R
+++ Hausaufgabe.R
@@ -1,3 +1,5 @@
 # Julian Hildebrandt hat die Hausaufgabe erledigt.
 # Er hat sie sogar zwei mal erledigt.
 # Maxi Musterfrau hat die Hausaufgabe erledigt.
+# Anna Musterfrau hat die Hausaufgabe erledigt.
+# Bob hat die Hausaufgabe erledigt.
