8875990c86d92cc1d9f8f6560cb71fab3bd07afe
Noah Schmitt <noahschmitt@Noahs-MacBook-Air-4.local>
2024-10-13 20:13:22 +0200

    Hausaufgabe gemacht

--- Hausaufgabe.R
+++ Hausaufgabe.R
@@ -2,3 +2,4 @@
 # Julian Hildebrandt hat die Hausaufgabe nochmal gemacht. 
 # Philipp Brauner hat die Hausaufgabe gemacht. abc
 # Monique Beckers hat die Hausaufgabe gemacht.
+# Noah Schmitt hat die Hausaufgabe gemacht.
