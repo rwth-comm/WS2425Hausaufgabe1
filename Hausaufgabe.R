035a68d21cf6d21fd995daf21bc51aa323abac39
Emilia Momot <emiliamomot@MacBook-AI.local>
2024-10-14 22:02:06 +0200

    Hausaufgabe gemacht

--- Hausaufgabe.R
+++ Hausaufgabe.R
@@ -1,14 +1,9 @@
 # Julian Hildebrandt hat die Hausaufgabe gemacht. 
 # Julian Hildebrandt hat die Hausaufgabe nochmal gemacht. 
-<<<<<<< HEAD
 # Philipp Brauner hat die Hausaufgabe gemacht. 
-# testabcdsfgh
-# Emilia Momot hat die Hausaufgabe gemacht.
-=======
 # Philipp Brauner hat die Hausaufgabe gemacht. abc
 # Monique Beckers hat die Hausaufgabe gemacht.
 # Noah Schmitt hat die Hausaufgabe gemacht.
 # Noah Schmitt hat die Hausaufgabe nochmal ohne Video gemacht 
 # Yordan Kirachen hat die Hausaufgabe gemacht.
->>>>>>> 03b00c892fb2f0c337c63dca57aacd51f0026dc9
 #Emilia Momot hat die Hausaufgabe gemacht.
