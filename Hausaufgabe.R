0f4c306d72461f9cd53e1da4d7327a3401a8b84c
ArjayanM <arjayan.moustafa@rwth-aachen.de>
2024-10-15 11:19:26 +0200

    Hausaufgabe gemacht

--- Hausaufgabe.R
+++ Hausaufgabe.R
@@ -7,3 +7,4 @@
 # Noah Schmitt hat die Hausaufgabe nochmal ohne Video gemacht 
 # Yordan Kirachen hat die Hausaufgabe gemacht.
 # Emilia Momot hat die Hausaufgabe gemacht.
+# Arjayan Moustafa hat die Hausaufgabe gemacht.
